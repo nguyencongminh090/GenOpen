446e995a3cbd9e2ca8f067f1b44fe6e23c47ad45
Nguyen Cong Minh <nguyencongminh050@gmail.com>
2021-05-04 09:37:17 +0700

    2/5/2021 + Finish generate function

--- Test_function.py
+++ Test_function.py
@@ -47,20 +47,20 @@ def xline(a, b):
     # Filter
     if a[0] == b[0]:
         i = 0
-        while i <= len(match)-1:
+        while i <= len(match) - 1:
             if match[i][0] == min(a, b)[0] and abs(match[i][1] - min(a, b)[1]) <= 5:
                 match.pop(i)
             else:
                 i += 1
     elif a[1] == b[1]:
         i = 0
-        while i <= len(match)-1:
+        while i <= len(match) - 1:
             if match[i][1] == min(a, b)[1] and abs(match[i][0] - min(a, b)[0]) <= 5:
                 match.pop(i)
             else:
                 i += 1
     i = 0
-    while i <= len(match)-1:
+    while i <= len(match) - 1:
         if abs(match[i][0] - a[0]) + 1 == 5 or abs(match[i][1] - a[1]) + 1 == 5:
             match.remove(match[i])
         elif abs(match[i][0] - b[0]) + 1 == 5 or abs(match[i][1] - b[1]) + 1 == 5:
@@ -70,33 +70,63 @@ def xline(a, b):
     return match
 
 
-def check(a, b, c):
+def check(a, b, c, n):
     # a and c is black stone
     # b is white stone.
-    print(a, c)
+    # n is size of board
     chk = xline(a, c)
-    chk.append(a)
-    chk.append(c)
-    print(chk)
-    for i in chk:
-        if i[1] == 0:
-            continue
-        # Solve
-        print(i)
-    pass
+    # Filter
+    # 1. Ignore close stone
+    # 1.1 Check a, b
+    t1 = True
+    if abs(a[0] - b[0]) + 1 < 3 or abs(a[1] - b[1]) + 1 < 3:
+        t1 = False - 1
+    t2 = True
+    if abs(b[0] - c[0]) + 1 < 3 or abs(b[1] - c[1]) + 1 < 3:
+        t2 = False - 1
+    t3 = True
+    if abs(a[0] - c[0]) + 1 <= 3 or abs(a[1] - c[1]) + 1 <= 3:
+        t3 = False
+    if t1 + t2 + t3 < 2:
+        return False
+    if chk:
+        chk.append(a)
+        chk.append(c)
+        for i in chk:
+            if i[1] == 0:
+                continue
+            # Solve
+            if abs(i[0] - b[0]) + 1 <= 6:
+                return True
+        if len(chk) == 4:
+            if a[0] == c[0] == 0 or a[1] == c[1] == 0 or a[0] == c[0] == n or a[1] == c[1] == n:
+                if b[0] == 0 or b[1] == 0 or b[0] == n or b[1] == n:
+                    if abs(a[1] - c[1]) + 1 <= 4 or abs(a[0] - c[0]) + 1 <= 4:
+                        return True
+    else:
+        if b[0] == 0 or b[1] == 0 or b[0] == n or b[1] == n:
+            # if abs(a[0] - b[0])
+            return True
+        return False
 
 
 def generate(n):
+    f = open('Output.txt', 'w')
     arr = []
-    for y in range(0, n-1):
+    for y in range(0, n + 1):
         for x in range(0, n + 1):
-            for y1 in range(y + 1, n):
-                for x1 in range(0, n+1):
-                    for y2 in range(y1 + 1, n + 1):
-                        for x2 in range(0, n+1):
-                            arr.append([[x, y], [x1, y1], [x2, y2]])
-    print(len(arr))
+            for y1 in range(0, n + 1):
+                for x1 in range(0, n + 1):
+                    for y2 in range(0, n + 1):
+                        for x2 in range(0, n + 1):
+                            if [x, y] != [x1, y1] != [x2, y2] and [x1, y1] != [x2, y2] != [x, y] \
+                                    and [x2, y2] != [x, y] != [x1, y1]:
+                                if check([x, y], [x1, y1], [x2, y2], n):
+                                    f.write('{}, {} - {}, {} - {}, {}\n'.format(x, y, x1, y1, x2, y2))
+                                    arr.append([[x, y], [x1, y1], [x2, y2]])
+    f.close()
+    print('Possible opening:', len(arr))
 
 
-# print(generate(14))
-print(check([7, 3], [6, 11], [2, 2]))
+print(generate(14))
+# print(check([3, 9], [14, 14], [14, 0], 14))
